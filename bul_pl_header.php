1b27fb4d17d67edab7055effbce0e60e5be60ad5
Kerri Hicks <kerri-hicks@users.noreply.github.com>
2020-02-21 11:41:33 -0500

    Added comments, extracted more CSS

--- bul_pl_header.php
+++ bul_pl_header.php
@@ -10,6 +10,7 @@ $config_rules_source = "" ;
 
 if(isset($bul_site_name)){
 
+	// map the site this is being used on to its configuration file
 	switch($bul_site_name){
 		case "bdr":
 			$config_file_location = "https://library.brown.edu/common/includes/configs/bdr.cfg" ;
@@ -32,9 +33,11 @@ if(isset($bul_site_name)){
 			break ;
 	}
 
+	// get the config file as a string and turn it into a JSON object represented as an array
 	$config_rules_source = file_get_contents($config_file_location) ;
 	$config_rules = json_decode($config_rules_source, true) ;
 	
+	// evaluate each JSON rule and assign values to reusable strings
 	foreach($config_rules as $key => $value) {
 		switch($key){		
 			case "background_color":
@@ -47,10 +50,12 @@ if(isset($bul_site_name)){
 				
 			case "site_title":
 				$site_title = $value ;
+				// used as a unique CSS id, but CSS ids can't have spaces
 				$site_title_nospaces = preg_replace('/\s+/', '_', $site_title) ;
 				break ;
 
 			case "site_short_title":
+				// used if needed between the words 'my' and 'account' (so, 'My BDR account' where short title for the Brown Digital Repository is 'bdr') 
 				$site_short_title = $value ; 
 				break ;
 
@@ -59,12 +64,14 @@ if(isset($bul_site_name)){
 				break ;
 
 			case "shib_site":
+				// either 'yes' or not yes
 				$shib_site = $value ; 
 				break ;
 
 			case "links":
 				$subnav_links = "" ;
 
+				// evaluate each item in the 'links' JSON object
 				foreach($value as $link) {	
 					
 					// this is a special case to toggle the a login/my account link for shib-enabled visitors...need to write JS to hide or show by ID if logged in
@@ -82,51 +89,39 @@ if(isset($bul_site_name)){
 						$subnav_links .= "<li id='login' class='bul_pl_subheader_single_link'><a href='$link[url]' style='text-decoration : none ; color : $link_color ;'>$link[label]</a></li>" ;
 					}
 					// end special case	
-												
+					
+					// if the 'links' item contains another 'links' item, to make a submenu
 					elseif(array_key_exists('links', $link)) {
-						 $subnav_links .= "<li class='bul_pl_subheader_links_trigger'><a href='javascript:;' onclick='javascript:void(0)' style='text-decoration : none ; color : $link_color ;'>$link[label]&nbsp;&nbsp;&dtrif;</a>" ;
+						// grab the top level label (which will be a dead link, but will be the header for the submenu)
+						$subnav_links .= "<li class='bul_pl_subheader_links_trigger'><a href='javascript:;' onclick='javascript:void(0)' style='text-decoration : none ; color : $link_color ;'>$link[label]&nbsp;&nbsp;&dtrif;</a>" ;
 						//  $trigram_menu_links .= "<li class='bul_pl_subheader_trigram_links'><a href='$link[url]' style='text-decoration : none ; color : $link_color ;'>$link[label]</a>" ;
+						
+						//create the submenu as a list
 						$subnav_links .= "<div class='bul_pl_subheader_sublinks_container'><ul class='bul_pl_subheader_sublinks'>" ;
 						foreach($link[links] as $sublink){
 							$subnav_links .= "<li><a href='$sublink[url]'>$sublink[label]</a></li>" ;
 							// $trigram_menu_links .= "<li class='bul_pl_subheader_trigram_links'><a href='$sublink[url]'>$sublink[label]</a></li>" ;
 						}
 						$subnav_links .= "</ul></div></li>" ;
-					}else{
+					}
+					// if there's no submenu, and it's just a single link
+					else{
 						$subnav_links .= "<li class='bul_pl_subheader_single_link'><a href='$link[url]' style='text-decoration : none ; color : $link_color ;'>$link[label]</a></li>" ;
 						// $trigram_menu_links .= "<li class='bul_pl_subheader_trigram_links'><a href='$link[url]'>$link[label]</a></li>" ;
 					}
 				}
-				$subnav_links .= "" ;
 				break ;
 			}		
 	  }
 	  
 echo "
 <!-- begin bul_pl_subheader -->
-
-
-<div id='bul_pl_subheader' style='
-	background-color : $background_color ; 
-	padding: 0px 0px 0px 25px ; 
-	font-family : CircularStd ; 
-	font-size : 13px ;		
-	box-sizing : border-box ;
-	width : 100% ; 
-	line-height : 1 ; 
-	display : grid ; 
-	grid-template-columns: auto auto ;
-	grid-template-rows: auto;
-	position: -webkit-sticky; /* Safari */
-	position: sticky;
-	top: 60px;' >
-		
+<div id='bul_pl_subheader' style='background-color : $background_color ; ' >
 	<div id='bul_pl_subheader_site_title'>
-		<a href='$site_url' style='text-decoration : none ; color : $link_color ;'>$site_title</a>
+		<a href='$site_url' style='color : $link_color ;'>$site_title</a>
 	</div>
 	<nav id='bul_pl_subheader_" . $site_title_nospaces . "_menu' class='bul_pl_subheader_nav_menu'>
 		<div id='bul_pl_subheader_site_links'>
-
 			<ul id='bul_pl_subheader_links'>
 				$subnav_links
 			</ul>	
