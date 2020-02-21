904cf7700aa4c4ccf811e5b54a13c9d00ff41854
Kerri Hicks <kerri-hicks@users.noreply.github.com>
2020-02-21 11:17:26 -0500

    Extracted CSS
    Pulled out embedded CSS where possible, inlined otherwise. Added nascent support for adaptive trigram menu. Added nascent support for login/my account support. Converted lengthening if statement to switch statement for performance.

--- bul_pl_header.php
+++ bul_pl_header.php
@@ -18,9 +18,17 @@ if(isset($bul_site_name)){
 		case "ocra":
 			$config_file_location = "https://library.brown.edu/common/includes/configs/ocra.cfg" ;
 			break ;
+
+		case "ocradata":
+			$config_file_location = "https://library.brown.edu/common/includes/configs/ocradata.cfg" ;
+			break ;
 	
 		case "josiah":
 			$config_file_location = "https://library.brown.edu/common/includes/configs/josiah.cfg" ;		
+			break ;
+			
+		case "libguides":
+			$config_file_location = "https://library.brown.edu/common/includes/configs/libguides.cfg" ;		
 			break ;
 	}
 
@@ -28,151 +36,105 @@ if(isset($bul_site_name)){
 	$config_rules = json_decode($config_rules_source, true) ;
 	
 	foreach($config_rules as $key => $value) {
-		if($key == 'background_color'){
-			$background_color = $value ;
-		}
-		elseif($key == 'link_color'){
-			$link_color = $value ;
-		}
+		switch($key){		
+			case "background_color":
+				$background_color = $value ;
+				break ;
 
-		elseif($key == 'site_title'){
-			$site_title = $value ;
-			$site_title_nospaces = preg_replace('/\s+/', '_', $site_title) ;
-		}
-		elseif($key == 'site_url'){
-			$site_url = $value ; 
-		}
-		elseif($key == 'links') {
-	    	$subnav_links = "<nav id='" . $site_title_nospaces . "_menu'><ul id='bul_pl_subheader_links'>" ;
-	    	foreach($value as $link) {			
-				if (array_key_exists('links', $link)) {
-					 $subnav_links .= "<li class='bul_pl_subheader_links_trigger'><a href='$link[url]'>$link[label]&nbsp;&nbsp;&dtrif;</a>" ;
-					// is there a url for the subnav link above? Should we check? Or just leave it?
-					$subnav_links .= "<div class='bul_pl_subheader_sublinks_container'><ul class='bul_pl_subheader_sublinks'>" ;
-					foreach($link[links] as $sublink){
-					    $subnav_links .= "<li><a href='$sublink[url]'>$sublink[label]</a></li>" ;
+			case "link_color":
+				$link_color = $value ;
+				break ;
+				
+			case "site_title":
+				$site_title = $value ;
+				$site_title_nospaces = preg_replace('/\s+/', '_', $site_title) ;
+				break ;
+
+			case "site_short_title":
+				$site_short_title = $value ; 
+				break ;
+
+			case "site_url":
+				$site_url = $value ; 
+				break ;
+
+			case "shib_site":
+				$shib_site = $value ; 
+				break ;
+
+			case "links":
+				$subnav_links = "" ;
+
+				foreach($value as $link) {	
+					
+					// this is a special case to toggle the a login/my account link for shib-enabled visitors...need to write JS to hide or show by ID if logged in
+					if($shib_site == "yes" && $link[label] == "My Account") {
+						$link[label] = "My $site_short_title Account" ;
+						
+						$subnav_links .= "<li class='bul_pl_subheader_links_trigger' id='bul_pl_subheader_myaccount'><a href='$link[url]' style='text-decoration : none ; color : $link_color ;'>$link[label]&nbsp;&nbsp;&dtrif;</a>" ;
+						$subnav_links .= "<div class='bul_pl_subheader_sublinks_container'><ul class='bul_pl_subheader_sublinks'>" ;
+						foreach($link[links] as $sublink){
+							$subnav_links .= "<li><a href='$sublink[url]'>$sublink[label]</a></li>" ;
+						}
+						$subnav_links .= "</ul></div></li>" ;
+					}					
+					elseif($shib_site == "yes"  && $link[label] == "Login") {
+						$subnav_links .= "<li id='login' class='bul_pl_subheader_single_link'><a href='$link[url]' style='text-decoration : none ; color : $link_color ;'>$link[label]</a></li>" ;
 					}
-					$subnav_links .= "</ul></div>" ;
-				}else{
-					$subnav_links .= "<li><a href='$link[url]'>$link[label]</a></li>" ;
+					// end special case	
+												
+					elseif(array_key_exists('links', $link)) {
+						 $subnav_links .= "<li class='bul_pl_subheader_links_trigger'><a href='javascript:;' onclick='javascript:void(0)' style='text-decoration : none ; color : $link_color ;'>$link[label]&nbsp;&nbsp;&dtrif;</a>" ;
+						//  $trigram_menu_links .= "<li class='bul_pl_subheader_trigram_links'><a href='$link[url]' style='text-decoration : none ; color : $link_color ;'>$link[label]</a>" ;
+						$subnav_links .= "<div class='bul_pl_subheader_sublinks_container'><ul class='bul_pl_subheader_sublinks'>" ;
+						foreach($link[links] as $sublink){
+							$subnav_links .= "<li><a href='$sublink[url]'>$sublink[label]</a></li>" ;
+							// $trigram_menu_links .= "<li class='bul_pl_subheader_trigram_links'><a href='$sublink[url]'>$sublink[label]</a></li>" ;
+						}
+						$subnav_links .= "</ul></div></li>" ;
+					}else{
+						$subnav_links .= "<li class='bul_pl_subheader_single_link'><a href='$link[url]' style='text-decoration : none ; color : $link_color ;'>$link[label]</a></li>" ;
+						// $trigram_menu_links .= "<li class='bul_pl_subheader_trigram_links'><a href='$link[url]'>$link[label]</a></li>" ;
+					}
 				}
-	    	}
-	    	$subnav_links .= "</ul></nav>" ;
-	    }
+				$subnav_links .= "" ;
+				break ;
+			}		
 	  }
 	  
-	echo "	
-		<style>
-		
-		div, ul {
-			border : 0px solid ; 
-		}
-		
-		#bul_pl_subheader {
-			background-color : $background_color ; 
-			padding: 0px 0px 0px 25px ; 
-			font-family : CircularStd ; 
-			font-size : 13px ;		
-			box-sizing : border-box ;
-			width : 100% ; 
-			line-height : 1 ; 
-			display : grid ; 
-			grid-template-columns: auto auto ;
-			grid-template-rows: auto;
-			position: -webkit-sticky; /* Safari */
-			position: sticky;
-			top: 60px;
-		}
+echo "
+<!-- begin bul_pl_subheader -->
 
-		#bul_pl_subheader a:focus {
-			outline : none ;
-			border-bottom : 1px solid ; 
-		}
-		
-		#bul_pl_subheader a {
-			text-decoration : none ;
-			color : $link_color ;
-		}
 
-		#bul_pl_subheader a:hover {
-			color : var(--brown_red) ; 
-		}
+<div id='bul_pl_subheader' style='
+	background-color : $background_color ; 
+	padding: 0px 0px 0px 25px ; 
+	font-family : CircularStd ; 
+	font-size : 13px ;		
+	box-sizing : border-box ;
+	width : 100% ; 
+	line-height : 1 ; 
+	display : grid ; 
+	grid-template-columns: auto auto ;
+	grid-template-rows: auto;
+	position: -webkit-sticky; /* Safari */
+	position: sticky;
+	top: 60px;' >
 		
-		#bul_pl_subheader_site_title {
-			display : inline-block ; 
-			padding : 13px 0px ;
-		}		
-		
-		#bul_pl_subheader_site_links {
-			display : inline-block ;
-			text-align : right ; 
-			padding-right : 25px ;
-		}
+	<div id='bul_pl_subheader_site_title'>
+		<a href='$site_url' style='text-decoration : none ; color : $link_color ;'>$site_title</a>
+	</div>
+	<nav id='bul_pl_subheader_" . $site_title_nospaces . "_menu' class='bul_pl_subheader_nav_menu'>
+		<div id='bul_pl_subheader_site_links'>
 
-		#bul_pl_subheader_links {
-			width : 100% ; 
-			box-sizing : border-box ;
-			display : inline-block ; 
-		}
-		
-		#bul_pl_subheader_links>li {
-			padding : 0px 15px ; 
-			box-sizing : border-box ;
-			display : inline-block ;
-		}
-		
-		#bul_pl_subheader_links>li a {
-			color : var(--brown_brown) ;
-			text-decoration : none ; 
-		}
-		
-		.bul_pl_subheader_sublinks_container {
-			display : none ;
-			position : absolute ;
-			box-sizing : border-box ;
-		}
-		
-		.bul_pl_subheader_sublinks {
-			min-width : 160px ;
-			box-shadow : 0px 8px 16px 0px rgba(0,0,0,0.2) ;
-			padding : 0px 10px 0px 10px ;
-			z-index : 0 ;
-			box-sizing : border-box ;
-			margin-left : -10px ; 
-			margin-top : 10px ; 
-			text-align : left ; 
-			background-color : #fff ; 
-			list-style-type : none ;
-		}
-		
-		.bul_pl_subheader_sublinks li {
-			border-bottom : 1px solid #F0F2F3 ;
-			padding : 20px 0px ;
-		}
-		
-		.bul_pl_subheader_sublinks li a {
-			text-decoration : none ; 
-			color : var(--brown_brown) ; 
-		}
-		
-		.bul_pl_subheader_links_trigger:hover .bul_pl_subheader_sublinks_container, 
-		.bul_pl_subheader_links_trigger:focus-within .bul_pl_subheader_sublinks_container {
-		  display : block ;
-		  box-sizing : border-box ;
-		}
-
-		</style>
-	
-		<div id='bul_pl_subheader'>
-			<div id='bul_pl_subheader_site_title'>
-				<a href='$site_url'>$site_title</a>
-			</div>
-			<div id='bul_pl_subheader_site_links'>
-				$subnav_links	
-			</div>
+			<ul id='bul_pl_subheader_links'>
+				$subnav_links
+			</ul>	
 		</div>
-	" ;
+	</nav>
+</div>
+<!-- end bul_pl_subheader -->
+" ;
 }
-
 
 ?>
